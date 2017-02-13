02285091e49ae54330a2a203392588f7a7e04dc1
NTI 48467 UVA <leomontenegro6@gmail.com>
2017-02-13 08:43:30 -0300

    Adicionado "gitignore" de modo a ignorar arquivos .bat e .sh

--- commitar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-read -p "Digite o texto do commit: `echo $'\n> '`" COMMIT
-
-#git config --global user.name "leomontenegro6"
-#git config --global user.email "leomontenegro6@gmail.com"
-git add .
-git commit -m "$COMMIT"
-git push origin master
