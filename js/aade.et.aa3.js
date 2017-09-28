681fc4ae4daddeb720d41dc456f192ab520b4def
leomontenegro6 <leomontenegro6@gmail.com>
2017-09-27 23:34:30 -0300

    - Adicionada tabela de equivalência de nomes / códigos do AA3.

--- js/aade.et.aa3.js
+++ js/aade.et.aa3.js
@@ -1,12 +1,140 @@
 /* Javascript library containing equivalence table for AA3 */
 
 aade.equivalenceTable = {
-	0: {
+	256: {
 		'original': '---',
 		'adapted': '---'
 	},
 	512: {
-		'original': 'Phoenix3',
-		'adapted': 'Roberto3'
+		'original': '---',
+		'adapted': '---'
+	},
+	10752: {
+		'original': 'Grossberg',
+		'adapted': 'Beaumont'
+	},
+	1792: {
+		'original': 'Mia',
+		'adapted': 'Amélia'
+	},
+	768: {
+		'original': 'Phoenix',
+		'adapted': 'Roberto'
+	},
+	1280: {
+		'original': 'Judge',
+		'adapted': 'Juíz'
+	},
+	2304: {
+		'original': 'Payne',
+		'adapted': 'Errantes'
+	},
+	2816: {
+		'original': 'Dahlia',
+		'adapted': 'Dália'
+	},
+	1024: {
+		'original': 'Maya',
+		'adapted': 'Alice'
+	},
+	7680: {
+		'original': 'Pearl',
+		'adapted': 'Margarida'
+	},
+	4096: {
+		'original': 'Andrews',
+		'adapted': 'Adriel'
+	},
+	13056: {
+		'original': 'Announcer',
+		'adapted': 'Anunciante'
+	},
+	1536: {
+		'original': 'Gumshoe',
+		'adapted': 'Gaspar'
+	},
+	3328: {
+		'original': 'Atmey',
+		'adapted': 'Zibido'
+	},
+	3072: {
+		'original': 'Ron',
+		'adapted': 'Ronaldo'
+	},
+	3584: {
+		'original': 'Desirée',
+		'adapted': 'Débora'
+	},
+	3840: {
+		'original': 'Butz',
+		'adapted': 'Vário'
+	},
+	4352: {
+		'original': 'Godot',
+		'adapted': 'Godot'
+	},
+	4608: {
+		'original': 'Byrde',
+		'adapted': 'Félix'
+	},
+	4864: {
+		'original': 'Armstrong',
+		'adapted': 'Brasforti'
+	},
+	14848: {
+		'original': 'Old Man',
+		'adapted': 'Velho'
+	},
+	5888: {
+		'original': 'Kudo',
+		'adapted': 'Renome'
+	},
+	6144: {
+		'original': 'Basil',
+		'adapted': 'Taner'
+	},
+	15616: {
+		'original': 'The Tiger',
+		'adapted': 'O Tigre'
+	},
+	5376: {
+		'original': 'Violetta',
+		'adapted': 'Violeta'
+	},
+	5120: {
+		'original': 'Tigre',
+		'adapted': 'Tigre'
+	},
+	6912: {
+		'original': 'Fawles',
+		'adapted': 'Mágoas'
+	},
+	2048: {
+		'original': 'Armando',
+		'adapted': 'Armando'
+	},
+	6656: {
+		'original': 'Edgeworth',
+		'adapted': 'Spada'
+	},
+	16128: {
+		'original': 'Melissa',
+		'adapted': 'Melissa'
+	},
+	8192: {
+		'original': 'Bikini',
+		'adapted': 'Biquíni'
+	},
+	7168: {
+		'original': 'Elise',
+		'adapted': 'Eliza'
+	},
+	8448: {
+		'original': 'Iris',
+		'adapted': 'Íris'
+	},
+	8704: {
+		'original': 'von Karma',
+		'adapted': 'do Carmo'
 	}
 };
