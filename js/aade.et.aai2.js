0f7b350fc4883ef23734c8b6b721819fc3c5d911
matheuscardoso96 <matheuscdsb@gmail.com>
2020-11-27 20:44:05 -0300

    Added initial support for Ace Attorney Investigations 2 script files

---
+++ js/aade.et.aai2.js
@@ -0,0 +1,181 @@
+/* Javascript library containing equivalence table for AAI2 */
+
+aade.equivalenceTable = {
+	0: {
+		'original': '---',
+		'adapted': '---'
+	},
+	1: {
+		'original': 'Edgeworth',
+		'adapted': 'Spada'
+	},
+	2: {
+		'original': 'Gumshoe',
+		'adapted': 'Gaspar'
+	},
+	3: {
+		'original': 'Bodyguard',
+		'adapted': 'Guarda-Costas'
+	},
+	4: {
+		'original': 'Forensics',
+		'adapted': 'Perícia'
+	},
+	5: {
+		'original': 'Nicole',
+		'adapted': 'Viviane'
+	},
+	6: {
+		'original': 'Payne',
+		'adapted': 'Dores'
+	},
+	7: {
+		'original': 'TV Crew',
+		'adapted': 'Equipe'
+	},
+	8: {
+		'original': 'Kay',
+		'adapted': 'Taís'
+	},
+	9: {
+		'original': 'Doe',
+		'adapted': 'Silva'
+	},
+	10: {
+		'original': 'Knightley',
+		'adapted': 'Cavalcante'
+	},
+	11: {
+		'original': 'de Killer',
+		'adapted': 'Mattar'
+	},
+	12: {
+		'original': 'Huang',
+		'adapted': 'Huang'
+	},
+	13: {
+		'original': 'Guard',
+		'adapted': 'Guarda'
+	},
+	14: {
+		'original': 'Ray',
+		'adapted': 'Léo'
+	},
+	15: {
+		'original': 'Prisoner',
+		'adapted': 'Prisioneiro'
+	},
+	16: {
+		'original': 'Elbird',
+		'adapted': 'Celas'
+	},
+	17: {
+		'original': 'Courtney',
+		'adapted': 'Cortês'
+	},
+	18: {
+		'original': 'Sebastian',
+		'adapted': 'Sebastião'
+	},
+	19: {
+		'original': 'Simon',
+		'adapted': 'Nico'
+	},
+	20: {
+		'original': 'Patricia',
+		'adapted': 'Patricia'
+	},
+	21: {
+		'original': 'Sahwit',
+		'adapted': 'Elvisso'
+	},
+	22: {
+		'original': 'Dogen',
+		'adapted': 'Falcão'
+	},
+	23: {
+		'original': 'Lang',
+		'adapted': 'Lang'
+	},
+	24: {
+		'original': 'Regina',
+		'adapted': 'Regina'
+	},
+	25: {
+		'original': 'Gregory',
+		'adapted': 'Gregório'
+	},
+	26: {
+		'original': 'Jeff',
+		'adapted': 'Guga'
+	},
+	27: {
+		'original': 'Kate',
+		'adapted': 'Cacá'
+	},
+	28: {
+		'original': 'Bradd',
+		'adapted': 'Durano'
+	},
+	29: {
+		'original': 'Gustavia',
+		'adapted': 'Ostos'
+	},
+	30: {
+		'original': 'Delicia',
+		'adapted': 'Dulce'
+	},
+	31: {
+		'original': 'von Karma',
+		'adapted': 'von Karma'
+	},
+	32: {
+		'original': 'Man',
+		'adapted': 'Homem'
+	},
+	33: {
+		'original': 'Mulher',
+		'adapted': 'Mulher'
+	},
+	34: {
+		'original': 'Butz',
+		'adapted': 'Vário'
+	},
+	35: {
+		'original': 'Karin',
+		'adapted': 'Karin'
+	},
+	36: {
+		'original': 'Franziska',
+		'adapted': 'Franizika'
+	},
+	37: {
+		'original': 'Brás',
+		'adapted': 'Blaise'
+	},
+	38: {
+		'original': 'Ema',
+		'adapted': 'Ema'
+	},
+	39: {
+		'original': 'Lotta',
+		'adapted': 'Sheila'
+	},
+	40: {
+		'original': 'Penny',
+		'adapted': 'Nicole'
+	},
+	41: {
+		'original': 'John',
+		'adapted': 'Artur'
+	},
+	42: {
+		'original': 'Powers',
+		'adapted': 'Ferraz'
+	},
+	43: {
+		'original': 'Driver',
+		'adapted': 'Motorista'
+	}	
+	
+};
